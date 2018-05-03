a54d80383e8715db08d0331fbe4211964fff3d14
Gaspard Bucher <gaspard@lucidogen.io>
2018-05-03 17:47:25 +0200

    Using prisma source with subscriptions.

--- src/index.js
+++ src/index.js
@@ -1,77 +1,58 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 
-function findLink(context, info, id) {
-  return context.db.query.links({ id }, info)
-}
-
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      // `info` contains the query AST so that the db
-      // knows what fields to return
-      return context.db.query.links({}, info)
-    },
-    link: (root, args, context, info) => {
-      return findLink(context, info, args.id)
+    feed(parent, args, ctx, info) {
+      return ctx.db.query.posts({}, info)
     },
   },
-
   Mutation: {
-    post(root, args, context, info) {
-      return context.db.query.createLink(
+    writePost(parent, { title }, ctx, info) {
+      return ctx.db.mutation.createPost(
         {
           data: {
-            url: args.url,
-            description: args.description,
+            title,
           },
         },
-        info
+        info,
       )
     },
-    updateLink(root, args) {
-      const link = findLink(args.id)
-      if (link) {
-        if (args.url) {
-          link.url = args.url
-        }
-        if (args.description) {
-          link.description = args.description
-        }
-      }
-      return link
-    },
-    deleteLink(root, args) {
-      const linkIdx = links.findIndex((l) => l.id === args.id)
-      if (linkIdx >= 0) {
-        const link = links[linkIdx]
-        delete links[linkIdx]
-        return link
-      }
-      return null
+    updateTitle(parent, { id, newTitle }, ctx, info) {
+      return ctx.db.mutation.updatePost(
+        {
+          where: {
+            id,
+          },
+          data: {
+            title: newTitle,
+          },
+        },
+        info,
+      )
     },
   },
-
-  Link: {
-    id: (root) => root.id,
-    description: (root) => root.description,
-    url: (root) => root.url,
+  Subscription: {
+    publications: {
+      subscribe: async (parent, args, ctx, info) => {
+        return ctx.db.subscription.post({}, info)
+      },
+    },
   },
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: (req) =>
-    Object.assign({}, req, {
-      db: new Prisma({
-        typeDefs: 'src/generated/prisma.graphql',
-        endPoint:
-          'https://eu1.prisma.sh/public-gravelhiss-219/hackernews-node/dev',
-        secret: '09asdfj20j',
-        debug: true,
-      }),
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'http://localhost:4466/subscriptions-example/dev',
+      secret: 'mysecret123',
     }),
+    debug: true,
+  }),
 })
+
 server.start(() => console.log(`Server is running on http://localhost:4000`))
